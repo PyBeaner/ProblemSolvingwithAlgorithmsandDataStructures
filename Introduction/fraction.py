7974aa2788aab34d864bf9e83443a9d3adade627
PyBeaner <758046738@qq.com>
2015-06-27 23:15:23 +0800

    lowest numerator and denominator

--- Introduction/fraction.py
+++ Introduction/fraction.py
@@ -16,6 +16,15 @@ class Fraction:
     def __init__(self, top, bottom):
         self.num = top
         self.den = bottom
+        self._common = gcd(self.num, self.den)
+
+    @property
+    def lowest_num(self):
+        return self.num // self._common
+
+    @property
+    def lowest_den(self):
+        return self.den // self._common
 
     def __str__(self):
         return str(self.num) + "/" + str(self.den)
@@ -24,14 +33,24 @@ class Fraction:
         print(self.num, "/", self.den)
 
     def __add__(self, other):
-        new_num = self.num * other.den + self.den * other.num
-        new_den = self.den * other.den
+        new_num = self.lowest_num * other.lowest_den + self.lowest_den * other.lowest_num
+        new_den = self.lowest_den * other.lowest_den
 
         common = gcd(new_num, new_den)
         return Fraction(new_num // common, new_den // common)
 
     def __eq__(self, other):
         return self.num * other.den == self.den * other.num
+
+    """
+    Implement the simple methods get_num and get_den that will return the numerator
+        and denominator of a fraction."""
+
+    def get_num(self):
+        return self.num
+
+    def get_den(self):
+        return self.den
 
 
 if __name__ == "__main__":
