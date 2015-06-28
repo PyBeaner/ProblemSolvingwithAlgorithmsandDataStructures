d850a130dea4ff3b863476957698a2b3e8e4f91e
PyBeaner <758046738@qq.com>
2015-06-28 12:22:05 +0800

    divide by n

--- Chapter 3. Basic Data Structures/divide_by_2.py
+++ Chapter 3. Basic Data Structures/divide_by_2.py
@@ -3,19 +3,26 @@ from stack import Stack
 
 
 def divide_by_2(dec_number):
+    return base_converter(dec_number, 2)
+
+
+def base_converter(dec_number, base):
+    digits = "0123456789ABCDEF"
     s = Stack()
-    n = 0
     while dec_number:
-        m, n = dec_number // 2, dec_number % 2
+        m, n = dec_number // base, dec_number % base
         s.push(n)
         dec_number = m
 
     binary_str = ""
     while not s.is_empty():
-        binary_str += str(s.pop())
+        binary_str += str(digits[s.pop()])
     return binary_str
 
 
 if __name__ == '__main__':
     binary_str = divide_by_2(256)
     print(binary_str)
+
+    print(base_converter(15 * 15, 16))
+    print(base_converter(8 * 10, 8))
