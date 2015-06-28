251a28cd7b9a0db755d4e959fa413aff49ff1051
PyBeaner <758046738@qq.com>
2015-06-28 11:58:49 +0800

    more

--- Chapter 3. Basic Data Structures/parentheses_balance.py
+++ Chapter 3. Basic Data Structures/parentheses_balance.py
@@ -6,17 +6,30 @@ def par_checker(symbol_string):
     s = Stack()
     balanced = True
     for char in symbol_string:
-        if char == "(":
+        if char in "([{":
             s.push(char)
         else:
             if s.is_empty():
                 balanced = False
                 break
             else:
-                s.pop()
+                left_char = s.pop()
+                if not parentheses_matches(left_char, char):
+                    balanced = False
+                    break
 
     return balanced and s.is_empty()
+
+
+def parentheses_matches(left, right):
+    opens = "({["
+    closes = ")}]"
+    return opens.find(left) == closes.find(right)
+
 
 print(par_checker("(((())))"))
 print(par_checker("(((()))"))
 print(par_checker("(()))"))
+
+print(par_checker('{{([][])}()}'))
+print(par_checker('[{()]'))
